6b42d4fd9949c02bf2971d5e4693ff5a3441e6f7
Ivan Storonsky <jstoronsky@gmail.com>
2023-06-10 02:27:24 +1200

    hw 17.1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,28 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE customers
+(
+    customer_id varchar(100) NOT NULL,
+    company_name varchar(100) NOT NULL,
+    contact_name varchar(100) NOT NULL
+    UNIQUE (customer_id)
+);
+
+
+CREATE TABLE employees
+(
+    employee_id int PRIMARY KEY,
+    first_name varchar(100) NOT NULL,
+    last_name varchar(100) NOT NULL,
+    title varchar(100) NOT NULL,
+    birthdate varchar(100) NOT NULL,
+    notes text
+);
+
+CREATE TABLE orders
+(
+    order_id int PRIMARY KEY,
+    customer_id_ varchar(100) REFERENCES customers(customer_id) NOT NULL,
+    employee_id_ int REFERENCES employees(employee_id) NOT NULL,
+    order_data varchar(100) NOT NULL,
+    ship_city varchar(100) NOT NULL
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,33 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+
+connect = psycopg2.connect(host='localhost', database='north', user='postgres', password='dem6600')
+
+with connect.cursor() as cur:
+    cur.execute('TABLE customers')
+    customers = cur.fetchall()
+    if len(customers) == 0:
+        cur.execute('''COPY customers(customer_id, company_name, contact_name)
+        FROM '/tmp/customers_data.csv'
+        DELIMITER ','
+    CSV HEADER;''')
+with connect.cursor() as cur_:
+    cur_.execute('TABLE employees')
+    employees = cur_.fetchall()
+    if len(employees) == 0:
+        cur_.execute('''COPY employees(employee_id, first_name, last_name, title, birthdate, notes)
+    FROM '/tmp/employees_data.csv'
+    DELIMITER ','
+    CSV HEADER;''')
+with connect.cursor() as cur__:
+    cur__.execute('TABLE orders')
+    orders = cur__.fetchall()
+    if len(orders) == 0:
+        cur__.execute('''COPY orders(order_id, customer_id_, employee_id_, order_data, ship_city)
+    FROM '/tmp/orders_data.csv'
+    DELIMITER ','
+    CSV HEADER;''')
+
+
+connect.commit()
+connect.close()
